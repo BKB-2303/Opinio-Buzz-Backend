03cf1f81975b0c3586c325a3012b2527517db06a
BKB-2303 <borahbikash2303@gmail.com>
2024-07-01 16:09:34 +0530

    middleware edit

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,28 +1,4 @@
 
-
-//old code
-// import jwt from "jsonwebtoken";
-// import { User } from "../Models/User.js";
-
-// export const Authenticated = async (req, res, next) => {
-//   const token = req.header("Auth");
-
-//   if (!token) return res.json({ message: "Login first" });
-
-//   const decoded = jwt.verify(token, "!@#$%^&*()");
-
-//   const id = decoded.userId;
-
-//   let user = await User.findById(id);
-
-//   if (!user) return res.json({ message: "User not exist" });
-
-//   req.user = user;
-//   next();
-
-//   // console.log(decoded)
-// };
-
 import jwt from "jsonwebtoken";
 import { User } from "../Models/User.js";
 
@@ -47,3 +23,26 @@ export const Authenticated = async (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized: Invalid token" });
   }
 };
+//old code
+// import jwt from "jsonwebtoken";
+// import { User } from "../Models/User.js";
+
+// export const Authenticated = async (req, res, next) => {
+//   const token = req.header("Auth");
+
+//   if (!token) return res.json({ message: "Login first" });
+
+//   const decoded = jwt.verify(token, "!@#$%^&*()");
+
+//   const id = decoded.userId;
+
+//   let user = await User.findById(id);
+
+//   if (!user) return res.json({ message: "User not exist" });
+
+//   req.user = user;
+//   next();
+
+//   // console.log(decoded)
+// };
+
